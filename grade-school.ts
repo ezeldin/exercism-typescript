1348ba095eea1bde83f1162e8fa672c1d4a3ca3a
ezeldin <ezeldin_hussein@hotmail.com>
2022-08-22 19:56:00 +0300

    ...

--- grade-school.ts
+++ grade-school.ts
@@ -1,62 +1,32 @@
-
-export interface ISchoolDb {
-    gradeNumbr: string;
-    students: string[]
+export interface schoolDb {
+  [key: number]: string[];
 }
 
 export class GradeSchool {
+  private store: schoolDb;
 
-    private schoolDb: Map<string, string[]>;
+  constructor() {
+    this.store = {};
+  }
 
-    constructor() {
-        this.schoolDb = new Map([
-            ['colors', ['blue', 'red', 'green']],
-            ['languages', ['english', 'spanish', 'german']],
-        ]);
+  roster(): schoolDb {
+    let newStore: schoolDb = {};
+    for (const [grade, students] of Object.entries(this.store)) {
+      newStore[parseInt(grade)] = students.slice();
     }
+    return newStore;
+  }
 
-    roster() {
-        return Object.fromEntries(this.schoolDb)
-    }
+  add(name: string, gradeNumber: number) {
+    this.store[gradeNumber] = this.store[gradeNumber] || [];
+    this.store[gradeNumber].push(name);
+  }
 
-    // add(name: string, gradeNumber: number) {
-    //     let mygrade = this.schoolDb.find(g => g.gradeNumbr = gradeNumber);
-
-    //     if (!mygrade) {
-    //         this.schoolDb.push({ gradeNumbr: gradeNumber, students: [name] });
-    //     }
-    //     else {
-    //         this.schoolDb = this.schoolDb.map(g => {
-    //             if (g.gradeNumbr == gradeNumber) {
-    //                 return {
-    //                     gradeNumbr: g.gradeNumbr,
-    //                     students: [...g.students, name],
-    //                 }
-    //             }
-    //             else {
-    //                 return {
-    //                     gradeNumbr: g.gradeNumbr,
-    //                     students: g.students,
-    //                 }
-    //             }
-    //         });
-    //     }
-    // }
-
-    // grade(gradeNumbr: number) {
-    //     return this.schoolDb.find(g => g.gradeNumbr == gradeNumbr)?.students;
-    // }
+  grade(gradeNumbr: number) {}
 }
-
 
 let school = new GradeSchool();
 console.log(school.roster());
-// school.add("zelafy", 1);
-// console.log(school.roster());
+school.add('zelafy', 1);
+console.log(school.roster());
 // console.log(school.grade(3));
-
-
-
-
-
-
